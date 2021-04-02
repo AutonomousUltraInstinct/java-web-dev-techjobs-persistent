d19ef966929095eb171ece6cadcb95de677c81e9
AutonomousUltraInstinct <56056368+AutonomousUltraInstinct@users.noreply.github.com>
2021-04-01 20:15:04 -0500

    Fin

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,6 +1,39 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Employer extends AbstractEntity {
 
+    @NotBlank(message = "Location required.")
+    @Size(max = 50, message = "Location is too long. Try a shorter term.")
+    private String location;
+
+
+    @OneToMany
+    @JoinColumn(name = "employer_id")
+    private List<Job> jobs = new ArrayList<>();
+
+    public Employer(){}
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public List<Job> getJobs() {
+        return jobs;
+    }
+
+    public void setJobs(List<Job> jobs){
+        this.jobs = jobs;
+    }
 
 }
--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
@@ -1,5 +1,33 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.ManyToMany;
+import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Skill extends AbstractEntity {
 
+    @Size(max = 250, message = "This exceeds the maximum character limit.")
+    private String description;
+
+    @ManyToMany(mappedBy = "skills")
+    private final List<Job> jobs = new ArrayList<>();
+
+    public Skill(String description) {
+        this.description = description;
+    }
+
+    public Skill(){}
+
+    public List<Job> getJobs(){
+        return jobs;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
